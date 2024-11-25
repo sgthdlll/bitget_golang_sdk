7d7f772a628ad55a1287f3653705647ebb88be46
sgthdlll <471311479@qq.com>
2024-11-25 21:28:37 +0800

    1

--- config/config.go
+++ config/config.go
@@ -6,8 +6,8 @@ const (
 	BaseUrl = "https://api.bitget.com"
 	WsUrl   = "wss://ws.bitget.com/mix/v1/stream"
 
-	ApiKey        = ""
-	SecretKey     = ""
+	ApiKey        = "bg_80380ea901db43ccf5565e98fe7ae287"
+	SecretKey     = "9d98417d0106d17fc27d78e1d21394df9aff650eb2ce3926272ce0714c964c0e"
 	PASSPHRASE    = ""
 	TimeoutSecond = 30
 	SignType      = constants.SHA256
